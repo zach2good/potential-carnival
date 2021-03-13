b3f512714dbdb4b1d12a84ac54f01915d2c5d7a6
Zach Toogood <zrtoogood@gmail.com>
2021-03-13 19:50:46 +0200

    Client listen for replies

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,10 +1,11 @@
 #include "BearLibTerminal.h"
-#include <string>
-#include <deque>
-#include <chrono>
 #include <array>
+#include <chrono>
 #include <cmath>
+#include <deque>
 #include <iostream>
+#include <string>
+#include <thread>
 
 #include "asio.hpp"
 
@@ -25,16 +26,16 @@ enum ItemType
 
 struct Item
 {
-    ItemType type;
+    ItemType    type;
     std::string ch;
     std::string color;
 };
 
 struct Char
 {
-    int x;
-    int y;
-    int facing;
+    int  x;
+    int  y;
+    int  facing;
     Item weapon;
     Item hat;
 };
@@ -64,30 +65,29 @@ void print_char(Char& c)
     terminal_layer(15);
     switch (c.facing)
     {
-    case 0: // Right
-        terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=16,0]/");
-        break;
-    case 1: // Down
-        // If facing Down, draw the weapon in front of the char
-        terminal_layer(17);
+        case 0: // Right
+            terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=16,0]/");
+            break;
+        case 1: // Down
+            // If facing Down, draw the weapon in front of the char
+            terminal_layer(17);
 
-        terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=10,5]/");
-        break;
-    case 2: // Left
-        terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=-16,0]\\");
-        break;
-    case 3: // Up
-        // If facing Up, draw the weapon behind the char
-        terminal_layer(10);
+            terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=10,5]/");
+            break;
+        case 2: // Left
+            terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=-16,0]\\");
+            break;
+        case 3: // Up
+            // If facing Up, draw the weapon behind the char
+            terminal_layer(10);
 
-        terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=0,-8]/");
-        break;
+            terminal_print_ext(c.x, c.y, 0, 0, 0, "[color=grey][offset=0,-8]/");
+            break;
     }
 }
 
 void cast_spell(int x, int y, Direction dir)
 {
-
 }
 
 struct Move
@@ -100,63 +100,102 @@ struct Move
 
 struct socket_t
 {
-    socket_t()
+    socket_t(std::string address_str, int port)
     : socket(io_context)
+    , endpoint(asio::ip::address::from_string(address_str), port)
     {
         socket.open(asio::ip::udp::v4());
+        socket.non_blocking(true);
     }
 
     ~socket_t()
+    {
+        close();
+    }
+
+    void close()
     {
         socket.close();
     }
 
     void send(std::vector<char> buffer)
     {
-        auto address = asio::ip::address::from_string("127.0.0.1");
-        asio::ip::udp::endpoint endpoint(address, 4444);
         socket.send_to(asio::buffer(buffer.data(), buffer.size()), endpoint);
     }
 
     void send_heartbeat(Char& ch)
     {
-        send({ 0x00, 0x00, 0x00, 0x00});
+        send({ 0x00, 0x00, 0x00, 0x00 });
     }
 
     void send_register_char(Char& ch)
     {
-        send({ 0x00, 0x00, 0x01, 0x00});
+        send({ 0x00, 0x00, 0x01, 0x00 });
     }
 
     void send_pos_update(Char& ch)
     {
-        send({ 0x00, 0x00, 0x02, 0x00});
+        send({ 0x00, 0x00, 0x02, 0x00 });
     }
 
-    asio::io_context io_context;
-    asio::ip::udp::socket socket;
+    std::vector<char> listen()
+    {
+        asio::ip::udp::endpoint sender;
+        std::vector<char>       buffer(1024);
+        asio::error_code        error             = asio::error::would_block;
+        std::size_t             bytes_transferred = 0;
+        while (error == asio::error::would_block)
+        {
+            bytes_transferred = socket.receive_from(asio::buffer(buffer), sender, 0, error);
+        }
+        buffer.resize(bytes_transferred);
+        return buffer;
+    }
+
+    asio::io_context        io_context;
+    asio::ip::udp::socket   socket;
+    asio::ip::udp::endpoint endpoint;
 };
 
 int main()
 {
-    // Data
-    const int fps = 30;
-    const float g_pi = 3.141592654f;
-    float angle = 0.0f;
     using namespace std::chrono_literals;
-    int ticks = 0;
-    auto last_tick = std::chrono::high_resolution_clock::now().time_since_epoch();
-    std::array<int, 80 * 25> world{'.'};
+
+    // Data
+    const int   fps       = 30;
+    const float g_pi      = 3.141592654f;
+    float       angle     = 0.0f;
+    int         ticks     = 0;
+    auto        last_tick = std::chrono::high_resolution_clock::now().time_since_epoch();
+
+    std::array<int, 80 * 25> world{ '.' };
 
     terminal_open();
     terminal_set("window: size=80x25; font: ./PressStart2P-Regular.ttf, size=16");
     terminal_composition(TK_ON);
 
     Char c{ 10, 10, 0, { WEAPON, "/", "grey" }, { HAT, "^", "red" } };
+
     std::deque<Move> move_queue;
 
+    int heartbeat = 0;
+
     // Networking
-    socket_t socket;
+    socket_t    socket("127.0.0.1", 4444);
+    bool        socket_listening = true;
+    std::thread socket_thread([&]() {
+        while (socket_listening)
+        {
+            auto reply = socket.listen();
+            if (!reply.empty())
+            {
+                if (reply.size() == 4 && reply[0] + reply[1] + reply[2] + reply[3] == 0)
+                {
+                    ++heartbeat;
+                }
+            }
+        }
+    });
 
     bool closing = false;
     while (!closing)
@@ -204,6 +243,8 @@ int main()
                 {
                     cast_spell(c.x, c.y, (Direction)c.facing);
                 }
+                break;
+                default:
                 break;
             }
         }
@@ -263,12 +304,12 @@ int main()
         // FG Effects (16)
         terminal_layer(16);
         int symbols = 12;
-        int radius = 2;
+        int radius  = 2;
         for (int i = 0; i < symbols; i++)
         {
             float angle_delta = 2.0f * g_pi / symbols;
-            float dx = std::cos(angle + i * angle_delta) * radius * terminal_state(TK_CELL_WIDTH);
-            float dy = std::sin(angle + i * angle_delta) * radius * terminal_state(TK_CELL_WIDTH) - 4;
+            float dx          = std::cos(angle + i * angle_delta) * radius * terminal_state(TK_CELL_WIDTH);
+            float dy          = std::sin(angle + i * angle_delta) * radius * terminal_state(TK_CELL_WIDTH) - 4;
             terminal_color(i % 2 ? "cyan" : "orange");
             terminal_put_ext(c.x, c.y, dx, dy, 'a' + i, nullptr);
         }
@@ -276,14 +317,17 @@ int main()
 
         // GUI
         terminal_color(color_from_name("white"));
-        terminal_printf(1, 1, "X: %i, Y: %i, Ticks: %i, q.size(): %i", c.x, c.y, ticks, move_queue.size());
-       
+        terminal_printf(1, 1, "X: %i, Y: %i, Ticks: %i, q.size(): %i, heartbeat: %i", c.x, c.y, ticks, move_queue.size(), heartbeat);
+
         // Submit
         terminal_refresh();
 
         // Sleep
         terminal_delay(1000 / fps);
     }
+    socket_listening = false;
+    socket.close();
+    socket_thread.join();
 
     terminal_close();
 }
